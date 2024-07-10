03c54722121e018f7543ca6f98055ab78bab992b
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-07-10 11:40:12 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'New Form'});
+});
+
+router.post("/new", function(req, res) {
+  const messageText = req.body.text;
+  const messageUser = req.body.user;
+
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/')
+})
+
+module.exports = router;
